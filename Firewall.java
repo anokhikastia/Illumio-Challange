6dceba77c6608b16cd6068c94ed142852ab4d675
YOUR NAME <YOUR EMAIL ADDRESS>
2018-08-25 16:08:36 -0700

    Committed

--- Firewall.java
+++ Firewall.java
@@ -3,31 +3,41 @@ import java.io.IOException;
 import java.io.FileNotFoundException; 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.List; 
+import java.util.Map; 
+import java.util.ArrayList; 
 
 /* Source: https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/ */
 
 public class Firewall {
+    List<HashMap<Integer, Integer>> list = new ArrayList<HashMap<Integer, Integer>>(); 
+    HashMap<Integer, Integer> m = new HashMap<Integer, Integer>(); //stores the hashcode. 
+    HashMap<Integer, String> largeRanges = new HashMap<Integer, String>(); //for storing large ranges. 
+    public int index = 0; 
 
-    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(); //stores the hashcode. 
     final static int PRIME = 92821; 
+    final static int MAX_SIZE = 1073741824; 
 
     public static void main(String[] args) {
-        Firewall f = new Firewall("networkrules.csv");
+        Firewall f = new Firewall("networkrules2.csv");
         boolean test = f.accept_packet("inbound", "tcp",80,"192.168.1.2");
         System.out.println(test);
         System.out.println(f.accept_packet("inbound", "udp", 53, "192.168.2.1"));
-        System.out.println(f.accept_packet("outbound", "tcp", 10234, "192.168.10.11"));
+        System.out.println(f.accept_packet("outbound", "tcp", 450, "192.168.10.11"));
         System.out.println(f.accept_packet("inbound", "tcp", 81, "192.168.1.2")); //false 
         System.out.println(f.accept_packet("inbound", "udp", 24, "52.12.48.92")); //false 
     }
 
     public Firewall(String f) {
         BufferedReader br = null;
+        list.add(m);
         try {
             br = new BufferedReader(new FileReader(f));
             String line; 
             while ((line = br.readLine()) != null) {
+                System.out.println(line);
                 String[] rule = line.split(",");
+
                 String direction = rule[0];
                 String protocol = rule[1];
                 String port = rule[2];
@@ -36,7 +46,8 @@ public class Firewall {
                     ip_address = ip_address.replace(".", "");
                     Rule r = new Rule(direction, protocol, Integer.parseInt(port), 
                         Long.parseLong(ip_address));
-                    map.put(r.getHash(), 1);
+                    HashMap<Integer, Integer> m = list.get(index);
+                    m.put(r.getHash(), 1);
                 }
                 if (port.contains("-") && !ip_address.contains("-")) { //port contains a range. 
                     String[] range = port.split("-");
@@ -50,9 +61,14 @@ public class Firewall {
                     String end = range[1].replace(".", "");
                     long s = Long.parseLong(start);
                     long e = Long.parseLong(end);
-                    addIPRange(s, e, direction, protocol, port);
+                    if ((e - s) <= 500) {
+                        addIPRange(s, e, direction, protocol, port);
+                        continue;
+                    }
+                    int hash = getLargeHash(direction, protocol, Integer.parseInt(port));
+                    largeRanges.put(hash, ip_address);
                 }
-                if (port.contains("-") && ip_address.contains("-")) {
+                if (port.contains("-") && ip_address.contains("-")) { //port and ip-address contains a range. 
                     String[] range = port.split("-");
                     int start = Integer.parseInt(range[0]); 
                     int end = Integer.parseInt(range[1]);
@@ -61,7 +77,14 @@ public class Firewall {
                     String ipend = iprange[1].replace(".", "");
                     long s = Long.parseLong(ipstart);
                     long e = Long.parseLong(ipend);
-                    addRanges(start, end, s, e, direction, protocol);
+                    if ((e - s) < 500) {
+                        addRanges(start, end, s, e, direction, protocol);
+                        continue;
+                    }
+                    for (int i = start; i <= end; i++) {
+                        int hash = getLargeHash(direction, protocol, i);
+                        largeRanges.put(hash, ip_address);
+                    }
                 }
             }
         } catch (FileNotFoundException e) {
@@ -79,11 +102,24 @@ public class Firewall {
         }
     }
 
+    public int getLargeHash(String direction, String protocol, int port) {
+        return 31 * (direction.hashCode() + protocol.hashCode() + port);
+    }
+
     public void addRanges(int start, int end, long s, long e, String direction, String protocol) {
         for (int i = start; i <= end; i++) { //port
             for (long j = s; j <= e; j++) {  //ip address. 
                 Rule r = new Rule(direction, protocol, i, j);
-                map.put(r.getHash(), 1);
+                HashMap<Integer, Integer> m = list.get(index);
+                if (m.size() >= MAX_SIZE) {
+                    HashMap<Integer, Integer> newMap = new HashMap<Integer, Integer>();
+                    list.add(newMap);
+                    index++; 
+                }
+                m = list.get(index);
+                m.put(r.hashCode(), 1);
+                list.remove(list.size() - 1);
+                list.add(m);
             }
         }
     }
@@ -91,7 +127,16 @@ public class Firewall {
     public void addIPRange(long s, long e, String direction, String protocol, String port) {
         for (long j = s; j <= e; j++) {
             Rule r = new Rule(direction, protocol, Integer.parseInt(port), j);
-            map.put(r.getHash(), 1);
+            HashMap<Integer, Integer> m = list.get(index);
+            if (m.size() >= MAX_SIZE) {
+                HashMap<Integer, Integer> newMap = new HashMap<Integer, Integer>();
+                list.add(newMap);
+                index++; 
+            }
+            m = list.get(index);
+            m.put(r.hashCode(), 1);
+            list.remove(list.size() - 1);
+            list.add(m);
         }
     }
 
@@ -99,7 +144,16 @@ public class Firewall {
         for (int i = start; i <= end; i++) {
             ip_address = ip_address.replace(".", "");
             Rule r = new Rule(direction, protocol, i, Long.parseLong(ip_address));
-            map.put(r.getHash(), 1);
+            HashMap<Integer, Integer> m = list.get(index);
+            if (m.size() >= MAX_SIZE) {
+                HashMap<Integer, Integer> newMap = new HashMap<Integer, Integer>();
+                list.add(newMap);
+                index++; 
+            }
+            m = list.get(index);
+            m.put(r.hashCode(), 1);
+            list.remove(list.size() - 1);
+            list.add(m);
         }
     }
 
@@ -108,7 +162,27 @@ public class Firewall {
         ipAddress = ipAddress.replace(".", "");
         Long addr = Long.parseLong(ipAddress);
         Rule r = new Rule(direction, protocol, port, addr);
-        return map.containsKey(r.getHash()); 
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            HashMap<Integer, Integer> map = list.get(i);
+            if (map.containsKey(r.hashCode())) {
+                return true; 
+            }
+        }
+        int hash = getLargeHash(direction, protocol, port);
+        if (!largeRanges.containsKey(hash)) {
+            return false; 
+        }
+        String ipAddr = largeRanges.get(hash);
+        String[] range = ipAddr.split("-");
+        String ipstart = range[0].replace(".", "");
+        String ipend = range[1].replace(".", "");
+        long start = Long.parseLong(ipstart); 
+        long end = Long.parseLong(ipend);
+        if (addr >= start && addr <= end) {
+            return true; 
+        }
+        return false; 
     }
 
     public class Rule {
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 I created a Rule class which would put each of these rules in a hashamp. I converted the Rule into a hashcode and put it in the hashmap.
 I iterated over the csv and added to the hashmap based on whether they contained ranges or ip-addresses. The way I tested it was by creating different csv files and then testing it by passing in different rules to the accept_packet method. If I had more time, I would have added checks to make sure that the ip-address and ports are valid. I would also have added more csv files and tested more. 
+
+If the ranges are large, especially for ip addresses, it takes a while for the hashmap to form. However, once the map is created, you can 
+easily check if a range in valid or not in O(1) time. 
